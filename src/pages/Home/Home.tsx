d9c3ebe44c086a3d0ec66099e5423ecbebe984f8
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2023-10-03 17:14:39 -0300

    protótipo da homepage e demais estruturas do código

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='container'>
+
+        <div>
+          <h3>WebMapa</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut justo eget arcu cursus varius. Quisque suscipit lacus at eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut justo eget arcu cursus varius. Quisque suscipit lacus at eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut justo eget arcu cursus varius. Quisque suscipit lacus at eleifend.</p>
+        </div>
+
+        <iframe
+            title="Mapa"
+            src="..\src\assets\map\MapaAreaContemplada.html"
+        ></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default Home
