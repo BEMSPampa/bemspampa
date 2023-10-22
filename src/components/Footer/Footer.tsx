0d44f36769bc83792dd2076cdf6e15b733fd9874
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2023-10-22 16:14:22 -0300

    Footer atualizado

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,35 @@
 import './Footer.scss'
-const Footer = () => {
-  return (
-    <div className='footer'>
+import pic from '../../assets/emailpic.png';
+import React, { useState } from 'react';
 
-    </div>
+const Footer = () => {
+
+  const email = 'bemspampa@gmail.com';
+
+  const [copied, setCopied] = useState(false);
+  const copyToClipboard = () => {
+    const inputElement = document.createElement('input');
+    inputElement.value = email;
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    inputElement.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(inputElement);
+    
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+
+  return (
+    <section className='footer'>
+      <h2>Contatos</h2>
+      <img onClick={copyToClipboard} src={pic} />
+      {copied && <p>Email copiado!</p>}
+    </section>
   )
 }
 
