f7930825c4ebb8d4b902a7971cf5f061f5dbf6b3
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2023-10-22 15:48:56 -0300

    mudanças na página sobre

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,20 +4,8 @@ import pic from '../../assets/unipampapic.jpg';
 const Home = () => {
 
   return (
-    <div className='home'>
-    <img src={pic}></img>
-      <div className="home-container" id="#teste">
-
-        <h2>Em desenvolvimento</h2>
-
-        <div className="cards">
-          <div className="card"/>
-          <div className="card"/>
-          <div className="card"/>
-          <div className="card"/>
-          <div className="card"/>
-        </div>
-      </div>
+    <div className='home-container'>
+      <img src={pic}></img>
     </div>
   )
 }
