c57846ccc132c961ac2761f1c87f56f998f025d1
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2023-10-26 14:06:33 -0300

    mudança na página (dados)

--- src/pages/Data/Data.tsx
+++ src/pages/Data/Data.tsx
@@ -14,10 +14,6 @@ const Data = () => {
                         <h3>Parece que a página está</h3>
                         <h3>em desenvolvimento...</h3>
                     </div>
-                    <div>
-                        <h5>Enquanto isso, clique no botão</h5>
-                        <h5>para acessar os dados.</h5>
-                    </div>
                 </div>
                 <Link to={linkData} target="_blank"><button>Acessar Dados</button></Link>
             </div>
