d2b1d18b0644d7d6a8fa934e88023f0eb51ee5cc
Guilherme Caneda <guilhermeccaneda@gmail.com>
2024-02-13 03:21:58 -0300

    mudanca na pagina de dados

--- src/pages/Data/Data.tsx
+++ src/pages/Data/Data.tsx
@@ -1,23 +1,28 @@
 import './Data.scss';
 import { Link } from 'react-router-dom';
-import pic from '../../assets/engpic.png';
 
+import CloudPic from '../../assets/data/cloudpic.png';
 
 const Data = () => {
     const linkData = "https://drive.google.com/drive/folders/1HoyETMMzg0ocvCUmTVOgwyXmzgjMD4rf?usp=share_link";
-
+    type ListData = {
+        name: string,
+        link: string
+    }
+    const listData: ListData[] = [{name: "HÍDRICA", link: linkData}, {name: "SOLAR", link: linkData}, {name: "EÓLICA", link: linkData}, {name: "BIOENERGIA", link: linkData},
+    {name: "FÓSSIL", link: linkData}, {name: "CARVÃO MINERAL", link: linkData}, {name: "DADOS MUNICIPAIS", link: linkData}, {name: "CONSUMO DE ENERGIA", link: linkData}, 
+    {name: "POTENCIAL", link: linkData}, {name: "BALANÇO ENERGÉTICO", link: linkData}, {name: "SUBESTAÇÕES DE ENERGIA", link: linkData}, {name: "LINHAS DE TRANSMISSÃO", link: linkData}, 
+    {name: "GERAÇÃO DISTRIBUÍDA", link: linkData}];
     return (
         <div className='data-container'>
-            <div className='warning'>
-                <img src={pic}/>
-                <div>
-                    <div>
-                        <h3>Parece que a página está</h3>
-                        <h3>em desenvolvimento...</h3>
+            {listData.map(item => (
+                <Link to={item.link} target="_blank"><div className='data-item'>
+                    <img src={CloudPic} alt="" />
+                    <div className='data-item-description'>
+                        <h3>{item.name}</h3>
                     </div>
-                </div>
-                <Link to={linkData} target="_blank"><button>Acessar Dados</button></Link>
-            </div>
+                </div></Link>
+            ))}
         </div>
     )
 }
