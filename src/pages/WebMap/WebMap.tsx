d107d382b68932b2c41721ecf8081694de30e163
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2024-02-14 16:16:44 -0300

    mudanças na pagina webmapa

--- src/pages/WebMap/WebMap.tsx
+++ src/pages/WebMap/WebMap.tsx
@@ -26,29 +26,83 @@ const WebMap = () => {
       </div>
 
       <div className="webmap-links-layout">
+
         <div className="webmap-cardgroup">
           <div className="webmap-card-epe">
             <img src={EPE} alt="" />
             <p>EPE Interativa</p>
             <p>BEN Interativo</p>
           </div>
-          <div className="webmap-card-aneel"></div>
+          <div className="webmap-card-aneel">
+            <p>Dados Abertos</p>
+            <p>Assuntos</p>
+            <p>Segmento: Geração</p>
+            <p>Segmento: Transmissão</p>
+            <p>Segmento: Distribuição</p>
+            <p>Eficiência Energética</p>
+            <img src={ANEEL} alt="" />
+          </div>
         </div>
+
+
         <div className="webmap-cardgroup">
-          <div className="webmap-card-inmet"></div>
-          <div className="webmap-card-sema"></div>
+          <div className="webmap-card-inmet">
+            <img src={INMET} alt="" />
+            <p>Mapa de Estações</p>
+            <p>Banco de Dados Meteorológicos</p>
+          </div>
+          <div className="webmap-card-sema">
+            <p>Painéis Interativos</p>
+            <p>Painéis de Acompanhamento Licenças Ambientais</p>
+            <img src={SEMA} alt="" />
+          </div>
         </div>
+
+
         <div className="webmap-cardgroup">
-          <div className="webmap-card-ons"></div>
-          <div className="webmap-card-ibge"></div>
+          <div className="webmap-card-ons">
+            <p>Portal de Dados Abertos do ONS</p>
+            <p>Painel Dinâmico</p>
+            <p>Mapas</p>
+            <img src={ONS} alt="" />
+          </div>
+          <div className="webmap-card-ibge">
+            <img src={IBGE} alt="" />
+            <p>Produção e Extração Vegetal e da Silvicultura</p>
+            <p>Pesquisa da Pecuária e Municipal</p>
+            <p>Indicadores</p>
+          </div>
         </div>
+
+
         <div className="webmap-cardgroup">
-          <div className="webmap-card-plugshare"></div>
-          <div className="webmap-card-ccee"></div>
+          <div className="webmap-card-plugshare">
+            <img src={PLUGSHARE} alt="" />
+            <p>Mapa de Carregadores de VE</p>
+          </div>
+          <div className="webmap-card-ccee">
+            <p>Dados Abertos</p>
+            <p>Geração</p>
+            <p>Consumo</p>
+            <p>Leilão</p>
+            <p>Estudos Especiais</p>
+            <img src={CCEE} alt="" />
+          </div>
         </div>
+
+
         <div className="webmap-cardgroup">
-          <div className="webmap-card-ana"></div>
-          <div className="webmap-card-mme"></div>
+          <div className="webmap-card-ana">
+            <p>Catálogo de metadados</p>
+            <p>Bacias Hidrográficas</p>
+            <p>Dados Abertos</p>
+            <img src={ANA} alt="" />
+          </div>
+          <div className="webmap-card-mme">
+            <img src={MME} alt="" />
+            <p>Painel Interativo</p>
+            <p>Publicações</p>
+          </div>
         </div>
       </div>
     </section>
