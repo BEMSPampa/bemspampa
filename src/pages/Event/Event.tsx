98aefe209652428cb8ba412f430b975362d55112
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2024-02-17 17:47:16 -0300

    correcoes para deploy

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -1,23 +1,26 @@
 import './Event.scss'
-import {  events } from '../../UI/strings'
-import { useParams } from "react-router-dom";
+//import {  events } from '../../UI/strings'
+//import { useParams } from "react-router-dom";
 const Event: React.FC = () => {
 
-  const { url } = useParams();
+ /* const { url } = useParams();
   //const { state } = useLocation();
   //const event: EventData = state?.event;
 
   const eventData = events.find((event) => event.url === url);
   console.log(eventData);
 
-  return (
-    <div className="event-container">
+*/
+  /*
+   <div className="event-container">
       <h1>{eventData?.title}</h1>
       <h4>Prévia da matéria</h4>
       <h5>{eventData?.date}</h5>
       <img src={eventData?.image} />
       <h3>{eventData?.description}</h3>
-    </div>
+    </div>*/
+  return (
+   <div></div>
   )
 }
 
