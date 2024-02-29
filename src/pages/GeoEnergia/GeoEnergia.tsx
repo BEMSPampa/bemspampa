d05521b3c2461f41ee4d9d35ce9f1c946cd8e8fb
Guilherme Caneda Correia <guilherme.correia.pb@compasso.com.br>
2024-02-29 13:06:47 -0300

    menu temporario da pagina geoenergia

--- src/pages/GeoEnergia/GeoEnergia.tsx
+++ src/pages/GeoEnergia/GeoEnergia.tsx
@@ -3,13 +3,24 @@ import GeoEnergiaTermo from '@Components/GeoEnergiaTermo/GeoEnergiaTermo'
 import GeoEnergiaNuclear from '@Components/GeoEnergiaNuclear/GeoEnergiaNuclear'
 
 import './GeoEnergia.scss'
+import { useState } from 'react'
 
 const GeoEnergia = () => {
+  const [isVisible, setIsVisible] = useState(1);
+
+  const handleIsVisible = (page: number) => {
+    setIsVisible(page);
+  }
   return (
     <div className='geonergia-container'> 
-      <GeoEnergiaSolar/>
-      <GeoEnergiaTermo/>
-      <GeoEnergiaNuclear/>
+      <div className='ge-temporary'>
+        <button onClick={() => handleIsVisible(1)}>Solar</button>
+        <button onClick={() => handleIsVisible(2)}>Termoelétrica</button>
+        <button onClick={() => handleIsVisible(3)}>Nuclear</button>
+      </div>
+      {isVisible === 1 && <GeoEnergiaSolar/>}
+      {isVisible === 2 && <GeoEnergiaTermo/>}
+      {isVisible === 3 && <GeoEnergiaNuclear/>}
     </div>
   )
 }
